96095dc9672cfb2b9b2b6009446466d378024cc1
Joshua Kent <hyperxbeat@live.com>
2020-06-11 23:49:54 +0100

    updated version and executables, fixed bug in b.ps1

---
+++ b.ps1
@@ -0,0 +1,65 @@
+# Builds the program from source code
+# Uses g++ compiler by default, and compiles
+# to Windows and Linux as an executable.
+
+param([switch] $exec, [switch] $e)
+# exec - executes after building (-e)
+
+$LinuxCommand = "wsl -e" # executing in WSL
+
+$SourceFile_WIN = "source\main.cpp"
+$SourceFile_LNX = $SourceFile_WIN.Replace("\", "/")
+$BuildLoc_WIN = "GMN_win64.exe"
+$BuildLoc_LNX = "GMN_linux"
+
+$WaitTime = 1000 # wait time when executing in milliseconds
+
+if ($e -eq $true) {
+    $exec = $true # -e is shorthand for -exec
+}
+
+# Function to call when building:
+function Build {
+    param($BuildLocation = $null, $OS = "win64") # when BuildLocation is null, build to temporary location
+    
+    if ($BuildLocation -eq $null) {
+        if ($OS -eq "win64") {
+            $BuildLocation = $BuildLoc_WIN
+        } elseif ($OS -eq "linux") {
+            $BuildLocation = $BuildLoc_LNX
+        }
+    }
+
+    if ($OS -eq "win64") {
+        Write-Output "Building GuessMyNumber for Windows 64-bit at $BuildLocation..."
+    } elseif ($OS -eq "linux") {
+        Write-Output "Building GuessMyNumber for Linux at $BuildLocation..."
+    }
+    
+    try {
+        if ($OS -eq "win64") {
+            g++ $SourceFile_WIN -o $BuildLocation
+        } elseif ($OS -eq "linux") {
+            Invoke-Expression "$LinuxCommand g++ $SourceFile_LNX -o $BuildLocation"
+        }
+
+        Write-Output "Build complete."
+    }
+    catch {}
+}
+
+# MAIN BUILD:
+Build $BuildLoc_WIN # build to main location for windows
+Write-Output ""
+Build $BuildLoc_LNX "linux" # build to main location for linux
+Write-Output ""
+
+# EXECUTE:
+if ($exec -eq $true) {
+    Write-Output ""
+    Write-Output "Executing application..."
+
+    Start-Sleep -millisecond $WaitTime
+    
+    Invoke-Expression ".\$MainBuildLoc_WIN"
+}
--- source/headers/version.hpp
+++ source/headers/version.hpp
@@ -4,8 +4,8 @@
 #define PROGRAMTYPE "console" // whether run in console or window
 
 // version info
-#define GMN_VERSION "beta 1.2" // Guess My Number Version
+#define GMN_VERSION "beta 1.3" // Guess My Number Version
 #define UPDATE_DATE "2020-06-11" // date when last updated
-#define UPDATE_TIME "23:47" // time when last updated
+#define UPDATE_TIME "23:49" // time when last updated
 
 #endif
