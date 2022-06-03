9504c98a6477368dd177dc22e878f9f84c51a4d7
Josh <jkent6@outlook.com>
2022-06-04 00:40:06 +0100

    fixed version

--- source/headers/version.hpp
+++ source/headers/version.hpp
@@ -6,10 +6,10 @@
 #define PROGRAMTYPE "console" // whether run in console or window
 
 // version info
-#define GMN_VERSION "1.0.1" // Guess My Number Version
+#define GMN_VERSION "1.0.2" // Guess My Number Version
 #define GMN_AUTHOR "Joshua Kent" // author
-#define UPDATE_DATE "2022-06-03" // date when last updated
-#define UPDATE_TIME "19:30" // time when last updated
+#define UPDATE_DATE "2022-06-04" // date when last updated
+#define UPDATE_TIME "00:39" // time when last updated
 
 int version() {
     misc::clear();
