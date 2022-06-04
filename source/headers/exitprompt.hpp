8d94d79bcb1b009ddacc5919be5b094fb016e50c
Josh <jkent6@outlook.com>
2022-06-04 18:47:42 +0100

    added exit prompt in full

--- source/headers/exitprompt.hpp
+++ source/headers/exitprompt.hpp
@@ -4,20 +4,30 @@
 
 #include <iostream>
 #include <string>
+
+#include "misc.hpp"
 // TODO: create an actual exit prompt (confirm exit)
 
 int exit() {
     std::string answer;
+
+    misc::clear();
 
     std::cout << "Are you really sure you want to exit? (Y/N): ";
     std::cin >> answer;
     
     // convert answer to lowercase
     for (char &c : answer) {
-        c = c.to_lower();
+        c = tolower(c);
     }
 
-    return 0;
+    if (answer == "yes" || answer == "y") {
+        return 1; // yes
+    } else if (answer == "no" || answer == "n") {
+        return 0; // no
+    } else {
+        return exit(); // ask again
+    }
 }
 
 #endif
