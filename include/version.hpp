5be7f7dfea89c89d2082a9f42bf87907f18c425d
Josh <jkent6@outlook.com>
2022-06-05 12:28:15 +0100

    changed string syntax

--- include/version.hpp
+++ include/version.hpp
@@ -1,10 +1,10 @@
 #ifndef VERSION_HPP
 #define VERSION_HPP
 
-static const char VERSION[] = "1.2.2";
-static const char AUTHOR[] = "Joshua Kent";
-static const char UPDATE_DATE[] = "2022-06-04";
-static const char UPDATE_TIME[] = "21:13";
+static const char* VERSION = "1.2.3";
+static const char* AUTHOR = "Joshua Kent";
+static const char* UPDATE_DATE = "2022-06-05";
+static const char* UPDATE_TIME = "12:27";
 
 int version();
 
